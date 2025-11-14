1e7b08d1c0d89568f3ac438578a0d82b69227be4
meerasunil <sunim094@newschool.edu>
2025-11-14 01:20:22 -0500

    favicon

---
+++ pages/api/document.js
@@ -0,0 +1,18 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        {/* if you want PNG as well: */}
+        {/* <link rel="icon" type="image/png" href="/favicon.png" /> */}
+      </Head>
+
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
